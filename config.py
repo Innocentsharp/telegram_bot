3ab60e11d35237afb58f0eda04728ed1d6682510
Innocentsharp <90131992+Innocentsharp@users.noreply.github.com>
2023-06-30 13:36:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print("start download packs")
-
-os.system("python -m pip install aiogram --upgrade pip")
-os.system("pip install beautifulsoup4, requests, aiogram, snscrape, asyncio")
-print("instalation is ended. \n start work with bot")
