f212b9b4f98789e4c5eebff88fdae721997a80dd
Innocentsharp <90131992+Innocentsharp@users.noreply.github.com>
2023-06-30 13:30:28 +0300

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-admin_id=''
+import os
+
+print("start dowload packs")
+
+os.system("python -m pip install aiogram --upgrade pip")
+os.system("pip install beautifulsoup4, requests, aiogram, snscrape, asyncio")
+print("instalation is ended. \n start work with bot")
